3cc43223ae31d2c9550bc845110f891ef1405afd
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-08-22 21:48:37 +0200

    entities update

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TemplateEntity, UserEntity, FileEntity } from '@pdf-me/shared';
+import {
+  TemplateEntity,
+  UserEntity,
+  FileEntity,
+  LimitEntity,
+} from '@pdf-me/shared';
 
 @Module({
   imports: [
@@ -15,7 +20,7 @@ import { TemplateEntity, UserEntity, FileEntity } from '@pdf-me/shared';
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
-        entities: [UserEntity, TemplateEntity, FileEntity],
+        entities: [UserEntity, TemplateEntity, FileEntity, LimitEntity],
         synchronize: true,
         logging: true,
       }),
