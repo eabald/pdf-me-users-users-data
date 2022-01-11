e7d1ad23b1c543c328586c22248fcdceba233a08
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2022-01-11 18:19:12 +0100

    config interface added

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
+import { Config } from '@eabald/pdf-me-shared';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  const configService: ConfigService<Config> = app.get(ConfigService);
   const user = configService.get('RABBITMQ_USER');
   const password = configService.get('RABBITMQ_PASSWORD');
   const host = configService.get('RABBITMQ_HOST');
